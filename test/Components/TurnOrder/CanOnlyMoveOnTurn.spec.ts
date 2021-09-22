c13146c241594df91185ebdd2e774236d4025203
Andrew Peterson <cybernetico@gmail.com>
2021-09-22 16:33:26 +0100

    Additional logic and refactors.

---
+++ test/Components/TurnOrder/CanOnlyMoveOnTurn.spec.ts
@@ -0,0 +1,31 @@
+import 'mocha';
+import { expect } from 'chai';
+
+import { Chaos, Entity, Team, Vector } from '@chaos/core';
+
+import CanOnlyMoveOnTurn from '../../../src/Components/TurnOrder/CanOnlyMoveOnTurn';
+import Teams from '../../../src/Enums/Teams';
+
+describe('Can only move on turn', () => {
+  beforeEach(() => {
+    Chaos.reset();
+  })
+
+  it("Stops a piece from moving when it's not the piece's team's turn", () => {
+    const piece = new Entity({ metadata: { team: Teams.WHITE } });
+    const movement = piece.move({ to: new Vector(0, 0) });
+    new CanOnlyMoveOnTurn().modify(movement);
+    Chaos.currentTurn = undefined;
+    movement.decidePermission();
+    expect(movement.permitted).to.be.false;
+  });
+
+  it("Does not stop a piece from moving on it's turn", () => {
+    const piece = new Entity({ metadata: { team: Teams.WHITE } });
+    const movement = piece.move({ to: new Vector(0, 0) });
+    new CanOnlyMoveOnTurn().modify(movement);
+    Chaos.currentTurn = 
+    movement.decidePermission();
+    expect(movement.permitted).to.be.true;
+  });
+});
